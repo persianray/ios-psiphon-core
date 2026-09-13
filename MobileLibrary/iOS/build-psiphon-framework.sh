3f60564d6a85deeb3a8267a31204d0925d6ecfbf
PersianRay <313188194+persianray@users.noreply.github.com>
2026-09-13 02:43:35 +0200

    Modify build script for PsiphonTunnel framework
    Updated build script to set default BUILD_TAGS and maintain Go version requirements.

--- MobileLibrary/iOS/build-psiphon-framework.sh
+++ MobileLibrary/iOS/build-psiphon-framework.sh
@@ -13,7 +13,7 @@
 
 set -e -u -x
 
-if [ -z ${1+x} ]; then BUILD_TAGS=""; else BUILD_TAGS="$1"; fi
+if [ -z ${1+x} ]; then BUILD_TAGS="PSIPHON_DISABLE_INPROXY"; else BUILD_TAGS="$1"; fi
 
 # Modify this value as we use newer Go versions.
 GO_VERSION_REQUIRED="1.26.3"
